7480303705db07ed49ce77fe271da53018ee3657
Vaidotas Simkus <simkus.vaidotas@gmail.com>
2021-11-22 19:03:59 +0000

    added gifs to thumbnail script

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -2,3 +2,4 @@
 mkdir -p tn/publications/images
 mogrify -path tn/publications/images -thumbnail 160x160 publications/images/*.png
 mogrify -path tn/publications/images -thumbnail 160x160 publications/images/*.jpg
+mogrify -path tn/publications/images -thumbnail 160x160 publications/images/*.gif
